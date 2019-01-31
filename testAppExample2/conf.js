385008d3dfb6f783dc66e7518e09969321e55fad
roshchik97 <roshchik97@mail.ru>
2019-02-01 02:30:04 +0300

    5 tests

---
+++ testAppExample2/conf.js
@@ -0,0 +1,25 @@
+exports.config = {
+    framework: 'jasmine2',
+    onPrepare: function() {
+        var AllureReporter = require('jasmine-allure-reporter');
+        jasmine.getEnv().addReporter(new AllureReporter({
+            resultsDir: 'allure-results'
+        }));
+    },
+
+
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+
+    capabilities: {
+        browserName: 'chrome'
+    },
+
+    specs: ['specs/spec.js'],
+
+
+
+    jasmineNodeOpts: {
+        showColors: true,
+    }
+
+};
